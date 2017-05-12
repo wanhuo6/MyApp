2667ed72c8d0584632158bae9550913f566e1978
liuhuijie <edward_hs@163.com>
2017-05-12 11:04:46 +0800

    添加测试类控制目录结构

---
+++ app/src/main/java/com/ahuo/personapp/entity/other/OtherEntity.java
@@ -0,0 +1,10 @@
+package com.ahuo.personapp.entity.other;
+
+/**
+ * Created on 17-5-12
+ *
+ * @author liuhuijie
+ */
+
+public class OtherEntity {
+}
---
+++ app/src/main/java/com/ahuo/personapp/entity/request/TestResponse.java
@@ -0,0 +1,10 @@
+package com.ahuo.personapp.entity.request;
+
+/**
+ * Created on 17-5-12
+ *
+ * @author liuhuijie
+ */
+
+public class TestResponse {
+}
