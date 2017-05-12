97e95c9b0b5e62dee4ad8d0ca845ef80a8d3c336
liuhuijie <edward_hs@163.com>
2017-05-12 11:03:30 +0800

    添加测试类控制目录结构

---
+++ app/src/main/java/com/ahuo/personapp/ui/adapter/TestAdapter.java
@@ -0,0 +1,10 @@
+package com.ahuo.personapp.ui.adapter;
+
+/**
+ * Created on 17-5-12
+ *
+ * @author liuhuijie
+ */
+
+public class TestAdapter {
+}
---
+++ app/src/main/java/com/ahuo/personapp/ui/fragment/TestFragment.java
@@ -0,0 +1,10 @@
+package com.ahuo.personapp.ui.fragment;
+
+/**
+ * Created on 17-5-12
+ *
+ * @author liuhuijie
+ */
+
+public class TestFragment {
+}
